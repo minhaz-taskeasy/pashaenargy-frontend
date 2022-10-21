95cf7aef20e196bbfaf9063e4960a9ebb8f0a033
Md Rasedul Islam <86698952+rased100@users.noreply.github.com>
2022-10-22 04:19:49 +0600

    bannerContentButton

--- src/Pages/Home/BannerContent.js
+++ src/Pages/Home/BannerContent.js
@@ -9,42 +9,51 @@ const BannerContent = () => {
     return (
         <>
             <div className="hero bg-[#fff]">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src={banner32} className="w-auto h-[420px]" />
+                <div className="hero-content flex-col lg:flex-row">
                     <div
-                        className='w-auto h-[420px]'
+                        className='w-auto h-[25rem] flex items-center'
                         style={{
-                            backgroundImage: `url(${banner31})`
+                            backgroundImage: `url(${banner31})`,
+                            backgroundSize: "cover",
+                            // backgroundPosition: "center",
+                            backgroundRepeat: "no - repeat"
+
                         }}>
-                        <div className=' text-left w-[60%] '>
-                            <h1 className="text-5xl font-bold">Carnegie Light
-                                Armed Scone</h1>
+                        <div className='text-left w-[60%] pl-20'>
+                            <h3 className="text-[40px] font-bold">Carnegie Light
+                                Armed Scone</h3>
                             <p className="py-6">Retro open basket design wall light in a black finish,
                                 featuring an inner clear glass cylinder light shade.</p>
-                            <button className="btn btn-primary">SHOP NOW</button>
+                            <a className='shopBtnL' href="/"><button className='shopBtn'>SHOP NOW</button></a>
                         </div>
                     </div>
+                    <img src={banner32} className="w-auto h-[25rem]" />
                 </div>
             </div>
             <div className="hero bg-[#fff]">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-
                     <div
-                        className='w-auto h-[420px]'
+                        className='w-auto h-[25rem] flex items-center'
                         style={{
-                            backgroundImage: `url(${banner33})`
+                            backgroundImage: `url(${banner33})`,
+                            backgroundSize: "cover",
+                            // backgroundPosition: "center",
+                            backgroundRepeat: "no - repeat"
+
                         }}>
-                        <div className=' text-left w-[60%] '>
-                            <h1 className="text-5xl font-bold">Carnegie Light
-                                Armed Scone</h1>
+                        <div className='text-left w-[60%] pl-20'>
+                            <h3 className="text-[40px] font-bold">Carnegie Light
+                                Armed Scone</h3>
                             <p className="py-6">Retro open basket design wall light in a black finish,
                                 featuring an inner clear glass cylinder light shade.</p>
-                            <button className="btn btn-primary">SHOP NOW</button>
+                            <a className='shopBtnL' href="/"><button className='shopBtn'>SHOP NOW</button></a>
                         </div>
                     </div>
-                    <img src={banner34} className="w-auto h-[420px]" />
+                    <img src={banner34} className="w-auto h-[25rem]" />
                 </div>
             </div>
+
+
         </>
     );
 };
