b0098d7d3105903602ed62abc794aad6e22dd9cc
Md Rasedul Islam <86698952+rased100@users.noreply.github.com>
2022-10-21 23:16:09 +0600

    navbar

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>This is Home Page</h2>
+        </div>
+    );
+};
+
+export default Home;
